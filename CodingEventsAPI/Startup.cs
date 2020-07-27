e07b5f05bfa47480d96c47a220eb5d9945208fc3
William Morrison <williamchrismorrison@gmail.com>
2020-07-27 10:42:46 -0700

    "made it my own!"

--- CodingEventsAPI/Startup.cs
+++ CodingEventsAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace CodingEventsAPI {
             "v1",
             new OpenApiInfo {
               Version = "v1",
-              Title = "Coding Events API",
+              Title = "Morrison's Coding Events API",
               Description = "REST API for managing Coding Events"
             }
           );
